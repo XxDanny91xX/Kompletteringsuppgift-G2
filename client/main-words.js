08073e198598937dcae739db6d689eccc1bc085d
XxDanny91xX <151919719+XxDanny91xX@users.noreply.github.com>
2024-01-18 15:06:22 +0100

    changed "Author" to "company"

--- client/main-words.js
+++ client/main-words.js
@@ -22,7 +22,7 @@ async function search() {
     html += `
       <section>
         <h2 class= "red-title">${meta.title}</h2>
-        <p><b>Author:</b> ${meta.Author}</p>
+        <p><b>Author:</b> ${meta.company}</p>
         <p><b>Created by:</b> ${meta.createdBy}</p>  
         <p><b>Modified by:</b> ${word.metadata.modifiedBy}</p>
       </section>
