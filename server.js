ab9bbaf256838e1d87903fc04ea028d89fbfe442
XxDanny91xX <151919719+XxDanny91xX@users.noreply.github.com>
2024-01-25 10:33:25 +0100

    Little adjustments to the .js files to make double check that they work on the server.js

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-// import file system module
-// - used to read file names from the words folder
-
-
+// Import the express package/module
+// that let us start up a web server
 import express from 'express';
-
-
-// import the musicMetadata
-// npm module - used to read metadata from music files
 
 // Import the database driver
 import mysql from 'mysql2/promise';
-const app = express();
-app.use(express.static('client'));
 
+// Create a web server named app
+const app = express();
+
+// Serve all files in the folder client
+app.use(express.static('client'));
 
 // Start the server on a certain port
 // and write to the terminal which port...
@@ -20,38 +17,25 @@ app.listen(3000, () =>
   console.log('Listening on http://localhost:3000'));
 
 
+
+// Create a connection 'db' to the database
 const db = await mysql.createConnection({
-  host: '161.97.144.27', // 127.0.0.1 if you want to run local db
-  port: 8092,               // change to your port
+  host: '161.97.144.27',
+  port:  8092,
   user: 'root',
-  password: 'guessagain92',  // change to your password
+  password:  'guessagain92',
   database: 'grupp-db'
 });
 
-// A small function for a query
+// A small function for a database query
 async function query(sql, listOfValues) {
   let result = await db.execute(sql, listOfValues);
   return result[0];
 }
 
-// read all file names from the Music fodler
-//const words = await fs.readdirSync('words');
 
-// loop through all music files and read metadata
-//for (let word of words) {
-
-  // Get the metadata for the file
-  //let metadata = await wordsMetadata.parseFile('client/words/' + word);
-
-  // delete (in-memory) some parts of the metadata
-  // that we don't want in the database
-  // note: we are not deleteing anything in the files
-  //delete metadata.native;
-  //delete metadata.quality;
-  //delete metadata.common.disk;
-
-  // A search route to find music
-app.get('/api/words/:searchTerm/:searchType', async (request, response) => {
+// A search route to find images
+app.get('/api/images/:searchTerm/:searchType', async (request, response) => {
   // Get the search term from as a parameter from the route/url
   let searchTerm = request.params.searchTerm;
   // Get teh search type a sa parameter from the route/url
@@ -61,7 +45,40 @@ app.get('/api/words/:searchTerm/:searchType', async (request, response) => {
 
   let sql = `
    SELECT * 
-   FROM words
+   FROM images
+   WHERE LOWER(metadata -> '$.${searchType}') LIKE LOWER (?)
+  `;
+
+  // since the sql gets a bit different if you want to search all
+  // fix this with a if-clause replacing the sql
+  if (searchType == 'all') {
+    sql = `
+      SELECT *
+      FROM images
+      WHERE LOWER(metadata) LIKE LOWER (?)
+    `;
+  }
+
+  let result = await query(sql, ['%' + searchTerm + '%']);
+
+  // Send a response to the client
+  response.json(result);
+});
+
+
+
+// A search route to find music
+app.get('/api/music/:searchTerm/:searchType', async (request, response) => {
+  // Get the search term from as a parameter from the route/url
+  let searchTerm = request.params.searchTerm;
+  // Get teh search type a sa parameter from the route/url
+  let searchType = request.params.searchType;
+  // Make a database query and remember the result
+  // using the search term
+
+  let sql = `
+   SELECT * 
+   FROM music
    WHERE LOWER(metadata -> '$.common.${searchType}') LIKE LOWER (?)
   `;
 
@@ -70,13 +87,107 @@ app.get('/api/words/:searchTerm/:searchType', async (request, response) => {
   if (searchType == 'all') {
     sql = `
       SELECT *
-      FROM words
+      FROM music
       WHERE LOWER(metadata) LIKE LOWER (?)
     `;
   }
+
   let result = await query(sql, ['%' + searchTerm + '%']);
+
   // Send a response to the client
   response.json(result);
-  // Log the result of inserting in the database
-  console.log(file, result);
 });
+
+
+//
+
+
+// A search route to find PDF
+app.get('/api/pdfs/:searchTerm/:searchType', async (request, response) => {
+  // Get the search term from as a parameter from the route/url
+  let searchTerm = request.params.searchTerm;
+  // Get teh search type a sa parameter from the route/url
+  let searchType = request.params.searchType;
+  // Make a database query and remember the result
+  // using the search term
+
+  let sql = `
+   SELECT * 
+   FROM pdfs
+   WHERE LOWER(metadata -> '$.info.${searchType}') LIKE LOWER (?)
+  `;
+
+  // since the sql gets a bit different if you want to search all
+  // fix this with a if-clause replacing the sql
+  if (searchType == 'all') {
+    sql = `
+      SELECT *
+      FROM pdfs
+      WHERE LOWER(metadata) LIKE LOWER (?)
+    `;
+  }
+
+  let result = await query(sql, ['%' + searchTerm + '%']);
+
+  // Send a response to the client
+  response.json(result);
+});
+
+
+
+// A search route to find powerpoint
+app.get('/api/powerpoints/:searchTerm/:searchType', async (request, response) => {
+  // Get the search term from as a parameter from the route/url
+  let searchTerm = request.params.searchTerm;
+  // Get teh search type a sa parameter from the route/url
+  let searchType = request.params.searchType;
+  // Make a database query and remember the result
+  // using the search term
+
+  let sql = `
+   SELECT * 
+   FROM powerpoints
+   WHERE LOWER(metadata -> '$.title.${searchType}') LIKE LOWER (?)
+  `;
+
+  // since the sql gets a bit different if you want to search all
+  // fix this with a if-clause replacing the sql
+  if (searchType == 'all') {
+    sql = `
+      SELECT *
+      FROM powerpoints
+      WHERE LOWER(metadata) LIKE LOWER (?)
+    `;
+  }
+
+  let result = await query(sql, ['%' + searchTerm + '%']);
+
+  // Send a response to the client
+  response.json(result);
+});
+
+app.get('/api/words/:searchTerm/:searchType', async (request, response) => {
+  // Get the search term from as a parameter from the route/url
+  let searchTerm = request.params.searchTerm;
+  // Get teh search type a sa parameter from the route/url
+  let searchType = request.params.searchType;
+  let sql = `
+     SELECT * 
+     FROM words
+     WHERE LOWER(metadata -> '$.metadata.${searchType}') LIKE LOWER (?)
+    `;
+  
+    // since the sql gets a bit different if you want to search all
+    // fix this with a if-clause replacing the sql
+    if (searchType == 'all') {
+      sql = `
+        SELECT *
+        FROM words
+        WHERE LOWER(metadata) LIKE LOWER (?)
+      `;
+    }
+    let result = await query(sql, ['%' + searchTerm + '%']);
+    // Send a response to the client
+    response.json(result);
+  });
+  
